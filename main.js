7b9a44f547b6f308c467585a6d6e635546dc88bf
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-12-01 23:37:10 +0100

    innit

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 // Prénom : Baptiste
 // Groupe : 2285
 document.documentElement.classList.add("js-enabled");
+
+for (let i = 1; i<200; i = i+2){
+    document.querySelector("#app").insertAdjacentHTML("beforeend",`<li class=\"grid__item\">${i}</li>`);
+    if (i%3 === 0 && i%9 === 0){
+
+    }
+}
